ed6c775fe20646ac276df63280b5a4d27316422e
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-10-20 19:13:07 +0200

    Start work on parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    io::{self, Cursor, Read},
+    mem::MaybeUninit,
+};
+
+#[derive(Debug)]
+enum ParseError {
+    InvalidModule,
+    NotImplemented(&'static str),
+    IOError(io::Error),
+}
+impl From<io::Error> for ParseError {
+    fn from(value: io::Error) -> Self {
+        Self::IOError(value)
+    }
+}
+
+#[derive(Debug)]
+struct Module {}
+
+fn module(data: &mut Cursor<&[u8]>) -> Result<Module, ParseError> {
+    #[allow(clippy::uninit_assumed_init)]
+    let mut bytes: [u8; 4] = unsafe { MaybeUninit::uninit().assume_init() };
+    data.read_exact(&mut bytes)?;
+    for b in bytes {
+        print!("{b:0x} ");
+    }
+    println!();
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bin: &[u8] = include_bytes!("../examples/hello_world.wasm");
+    let mut cursor = Cursor::new(bin);
+
+    println!("{:?}", module(&mut cursor))
 }

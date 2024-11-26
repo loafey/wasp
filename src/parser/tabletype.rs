96368501e1d8312f80b7d4ff4efc771a1e62f728
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-11-26 22:05:20 +0100

    Add parsing for table types.

--- src/parser/tabletype.rs
+++ src/parser/tabletype.rs
@@ -1,15 +1,21 @@
-use super::Parsable;
+use super::{Limits, Parsable, RefTyp};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct TableType;
+pub struct TableType {
+    pub et: RefTyp,
+    pub lim: Limits,
+}
 impl Parsable for TableType {
     fn parse_inner(
-        _data: &mut std::io::Cursor<&[u8]>,
-        _: super::DebugStack,
+        data: &mut std::io::Cursor<&[u8]>,
+        stack: super::DebugStack,
     ) -> Result<Self, super::error::ParseError>
     where
         Self: std::marker::Sized,
     {
-        todo!()
+        Ok(TableType {
+            et: Parsable::parse(data, stack)?,
+            lim: Parsable::parse(data, stack)?,
+        })
     }
 }
